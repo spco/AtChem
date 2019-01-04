389775f5f4c16b7fc6a4dd92890288432bec336a
Sam Cox <sc676@le.ac.uk>
2019-01-04 14:18:36 +0000

    Initial stab at using dlopen instead of a function parser.

---
+++ src/example.f90
@@ -0,0 +1,80 @@
+program example
+    use :: iso_c_binding
+implicit none
+
+    integer(c_int), parameter :: rtld_lazy=1 ! value extracte from the C header file
+    integer(c_int), parameter :: rtld_now=2 ! value extracte from the C header file
+    !
+    ! interface to linux API
+    interface
+        function dlopen(filename,mode) bind(c,name="dlopen")
+            ! void *dlopen(const char *filename, int mode);
+            use iso_c_binding
+            implicit none
+            type(c_ptr) :: dlopen
+            character(c_char), intent(in) :: filename(*)
+            integer(c_int), value :: mode
+        end function
+
+        function dlsym(handle,name) bind(c,name="dlsym")
+            ! void *dlsym(void *handle, const char *name);
+            use iso_c_binding
+            implicit none
+            type(c_funptr) :: dlsym
+            type(c_ptr), value :: handle
+            character(c_char), intent(in) :: name(*)
+        end function
+
+        function dlclose(handle) bind(c,name="dlclose")
+            ! int dlclose(void *handle);
+            use iso_c_binding
+            implicit none
+            integer(c_int) :: dlclose
+            type(c_ptr), value :: handle
+        end function
+    end interface
+
+    ! Define interface of call-back routine.
+    abstract interface
+        subroutine called_proc (i, i2) bind(c)
+            use, intrinsic :: iso_c_binding
+            integer(c_int), intent(in) :: i
+            integer(c_int), intent(out) :: i2
+        end subroutine called_proc
+    end interface
+
+    ! testing the dynamic loading
+    integer i, i2
+    type(c_funptr) :: proc_addr
+    type(c_ptr) :: handle
+    character(256) :: pName, lName
+
+    procedure(called_proc), bind(c), pointer :: proc
+    !
+    i = 15
+
+    handle=dlopen("./test.so"//c_null_char, RTLD_LAZY)
+    if (.not. c_associated(handle))then
+        print*, 'Unable to load DLL ./test.so'
+        stop
+    end if
+    !
+    proc_addr=dlsym(handle, "t_times2"//c_null_char)
+    if (.not. c_associated(proc_addr))then
+        write(*,*) 'Unable to load the procedure t_times2'
+        stop
+    end if
+    call c_f_procpointer( proc_addr, proc )
+    call proc(i,i2)
+    write(*,*) "t_times2, i2=", i2
+    !
+    proc_addr=dlsym( handle, "t_square"//c_null_char )
+    if ( .not. c_associated(proc_addr) )then
+        write(*,*)'Unable to load the procedure t_square'
+        stop
+    end if
+    call c_f_procpointer(proc_addr, proc)
+    call proc(i,i2)
+    write(*,*) "t_square, i2=", i2
+contains
+end program example
